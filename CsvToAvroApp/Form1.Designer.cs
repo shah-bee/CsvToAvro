46629204186f0fab4c731c573661b29a0580c713
tahers <tahers@INHYDTAHERS>
2017-07-11 18:29:29 +0530

    Add project files.

---
+++ CsvToAvroApp/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace CsvToAvroApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertCsvToAvro = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectFiles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConvertCsvToAvro
+            // 
+            this.ConvertCsvToAvro.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ConvertCsvToAvro.Location = new System.Drawing.Point(493, 285);
+            this.ConvertCsvToAvro.Name = "ConvertCsvToAvro";
+            this.ConvertCsvToAvro.Size = new System.Drawing.Size(100, 25);
+            this.ConvertCsvToAvro.TabIndex = 0;
+            this.ConvertCsvToAvro.Text = "Convert";
+            this.ConvertCsvToAvro.UseVisualStyleBackColor = true;
+            this.ConvertCsvToAvro.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // selectFiles
+            // 
+            this.selectFiles.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.selectFiles.Location = new System.Drawing.Point(493, 104);
+            this.selectFiles.Name = "selectFiles";
+            this.selectFiles.Size = new System.Drawing.Size(100, 25);
+            this.selectFiles.TabIndex = 1;
+            this.selectFiles.Text = "select...";
+            this.selectFiles.UseVisualStyleBackColor = true;
+            this.selectFiles.Click += new System.EventHandler(this.selectFiles_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Conversion Tool";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(116, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(477, 144);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(674, 363);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectFiles);
+            this.Controls.Add(this.ConvertCsvToAvro);
+            this.Name = "Form1";
+            this.Text = "CsvToAvro";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConvertCsvToAvro;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button selectFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CsvToAvroApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsvToAvroApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectFiles_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.ShowDialog();
+
+            DataTable dt = new DataTable();
+            DataRow dr;
+            if (openFileDialog1.ShowDialog() != DialogResult.None)
+            {
+                dt.Columns.Add("FileName");
+                foreach (var fileName in openFileDialog1.FileNames)
+                {
+                    dr = dt.NewRow();
+                    dr["FileName"] = fileName;
+                    dt.Rows.Add(dr);
+                }
+                dataGridView1.DataSource = dt;
+            }
+        }
+    }
+}
